65b3e6625f9103d655d2c50e0286edfa4d1c99e8
Leonie Meiners <leonie_meiners@outlook.de>
2022-11-25 09:00:56 +0100

    Added script that computes zscores on a particular column in a dataframe after grouping.

---
+++ dfGroupbyApply.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+import pandas as pd
+
+data = {'item': ('pants', 'pants', 'shirt', 'sweater', 'sweater', 'sweater'),
+		'price': (40, 45, 15, 30, 25, 20)}
+
+df = pd.DataFrame(data)
+
+standardize = lambda series: (series - series.mean())/series.std()
+
+df['zPrice'] = df.groupby('item').price.transform(standardize)
+
+print(df)
